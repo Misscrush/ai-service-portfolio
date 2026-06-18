5f947c0fab4055d911b88f201c8bcfcc3b56c655
yecru <90256239+Misscrush@users.noreply.github.com>
2026-06-18 21:10:10 +0800

    Remove malformed samples\landing-page\script.js

--- samples\landing-page\script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("theme").addEventListener("click", () => {
-  const root = document.documentElement;
-  root.dataset.theme = root.dataset.theme === "dark" ? "" : "dark";
-});
-
