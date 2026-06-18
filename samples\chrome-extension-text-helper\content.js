51dc1350e5c9f9c6fdd570cca5e31a2bcfc0dfad
yecru <90256239+Misscrush@users.noreply.github.com>
2026-06-18 21:10:04 +0800

    Remove malformed samples\chrome-extension-text-helper\content.js

--- samples\chrome-extension-text-helper\content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-  if (message.type === "GET_SELECTION") {
-    sendResponse({ text: window.getSelection().toString() });
-  }
-});
-
